8becd809302f25670c69ad6f76a3a2bc03085d1b
OleksandrGudenko <oleksandr.gudenko@gmail.com>
2020-02-01 00:10:13 +0200

    Added POST API endpoints for all collections.

--- server.js
+++ server.js
@@ -1,55 +1,133 @@
-
+// API ChatApp v1.0
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 
 const MongoClient = require('mongodb').MongoClient;
 
-const url = 'mongodb://127.0.0.1:27017'
-// const url2 = 'mongodb://newtechproject.ddns.net:27017'
+const url = 'mongodb://127.0.0.1:27017';
 
-var db
+let db;
 
-MongoClient.connect(url, (err, client) => {
-    if (err) return console.log(err)
-    db = client.db('DB_ChatAPP')
+MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
+    if (err) return console.log(err);
+    db = client.db('DB_ChatAPP');
     
-   // let api = require('./routes/api/Chat/chat_app_api_v1');
-   // app.use('/api/', api);
-    
-    app.listen(4000)
+    app.listen(4000);
 })
 
 
-   
+// API end points starts here 
+
 app.get('/chats', (req, res) => {
-    var cursor = db.collection('Groups').find().toArray(function(err, results) {
-       res.send(results)
-       })
+    db.collection('Chats').find().toArray(function(err, results) {
+       res.send(results);
+    })
        
 })
+.post('/chats', (req, res) => {
+	let chatName = req.body.chatName;
+	let displayName = req.body.displayName;
+	let isPublic = req.body.isPublic;
+	
+	db.collection('Chats').findOne({"chatName": chatName}, function(err, result) {
+		if(err || result == null) {
+			db.collection('Chats').insert({"chatName": chatName, "displayNames": [displayName], "isPublic": isPublic}, function(err, insertResult) {
+				if(!err){ 
+					res.send(insertResult);
+				}
+				else {
+					res.send(err);
+				}
+			})
+		} else {
+			res.send("Sorry, Chat name is alredy in use.");
+		}
+	})
+})
 .get('/displaynames', (req, res) => {
-    var cursor = db.collection('DisplayNames').find().toArray(function(err, results) {
-       res.send(results)
-       })
+    db.collection('DisplayNames').find().toArray(function(err, results) {
+       res.send(results);
+    })
+})
+.post('/displaynames', (req, res) => {
+	let displayName = req.body.displayName;
+	let publicChats = [ "Politics", "Religion", "Cars", "Pro Vegan", "Art", "Technology", "Android vs IOS", "PC vs Consoles", "Gamers" ];
+	console.log(displayName)
+	db.collection('DisplayNames').findOne({"displayName" : displayName}, function(err, result) {
+		console.log(err, result)
+		if(err && result == null){
+			db.collection('DisplayNames').insertOne({"displayName" : displayName, "chats" : publicChats, "inUse" : true}, function(err, results) {
+				if(!err){ 
+					res.send(err);
+				}
+				else {
+					res.send(results);
+				}
+			})
+		} else {
+			res.send("Sorry, name is alredy in use.");
+		}
+	})
+
 })
 .get('/messages', (req, res) => {
-    var reqchat = req.query.chat;
-    console.log(reqchat)
-    if (reqchat != null) {
-        var cursor = db.collection('Messages').find({chat : reqchat }).toArray(function(err, results) {
-            res.send(results)
+    let reqchat = req.query.chat;
 
-            })
-    } else {
-        var cursor = db.collection('Messages').find({}).toArray(function(err, results) {
+    if (reqchat != null) {
+        db.collection('Messages').find({chat : reqchat }).toArray(function(err, results) {
             res.send(results)
-            })
+		})
+    } else {
+        db.collection('Messages').find({}).toArray(function(err, results) {
+            res.send(results)
+		})
     }
 
+})
+.post('/messages', (req, res) => {
+	let chat = req.query.chat;
+	let displayName = req.query.displayName;
+	let message = req.body.message;
+	
+
+	db.collection('Chats').findOne({"chatName": chat}, function(err, findRes) {		
+		let BreakException = {};
+		
+		if (findRes.isPublic != true) {
+			try {
+				findRes.displayNames.forEach( function(element) {
+					if( element == displayName ) {
+						db.collection('Messages').insertOne({"chat": chat, "displayName": displayName, "message": message}, function(err, insertRes) {
+							if(err) {
+								res.send(err)
+								console.log(err)
+
+							} else {
+								res.send(insertRes.message)
+							}
+						})
+						throw BreakException;
+					}
+				})
+			} catch (e) {
+				if (e != BreakException) res.send("ServerError, message not sent.")
+			}
+		} else {
+			db.collection('Messages').insertOne({"chat": chat, "displayName": displayName, "message": message}, function(err, insertRes) {
+				if(err) {
+					res.send(err)
+				} else {
+					res.send(insertRes.message)
+				}
+			})	
+		} 
+	})
 })
 
 
